d998347cb34236b0813df49983b8252c810e88b3
Takuya <uoastudent182@gmail.com>
2021-09-19 15:47:38 +0900

    落とし物のuserとitem取得し返す

--- app/controllers/api/v1/lost_items_controller.rb
+++ app/controllers/api/v1/lost_items_controller.rb
@@ -1,11 +1,22 @@
 class Api::V1::LostItemsController < ApplicationController
   def show
     item_id = params[:id]
-    item = Item.find(item_id)
-    user = item.user
-    render json: {
-      item: item,
-      user: user
-    }
+    begin
+      item = Item.find(item_id)
+      user = item.user
+      render json: {
+        lostItemData:{
+          item: item,
+          user: user
+        },
+        found: true
+      }
+    rescue => exception
+      render json: {
+        found: false,
+        error: exception
+      }
+    end
+
   end
 end
