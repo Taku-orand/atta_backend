b584f8de9415ffea7cf6f00334e65979949de719
Takuya <uoastudent182@gmail.com>
2021-09-22 14:07:14 +0900

    qr_codeをtextに修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_125547) do
+ActiveRecord::Schema.define(version: 2021_09_22_042627) do
 
   create_table "items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "name"
     t.text "content"
-    t.string "qr_code"
+    t.text "qr_code"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
